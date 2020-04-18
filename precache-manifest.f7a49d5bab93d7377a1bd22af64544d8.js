288c4e0459d434ee76875797bfa4a092626444e7
Valentyn <valentine.oleynik@gmail.com>
2020-04-18 15:36:46 +0300

    Updates

---
+++ precache-manifest.f7a49d5bab93d7377a1bd22af64544d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23e8b1588fe80eede0b2e95e6d91f96",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "640475a33d05683dd1e8",
+    "url": "/covid-19/static/css/main.cbe83677.chunk.css"
+  },
+  {
+    "revision": "7b2e84abbac7096fc67c",
+    "url": "/covid-19/static/js/2.3ff8fe35.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.3ff8fe35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640475a33d05683dd1e8",
+    "url": "/covid-19/static/js/main.3984eb71.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
